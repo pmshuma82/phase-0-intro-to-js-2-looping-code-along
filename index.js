a5beb010d0ba2b940ae6b8449337e0d0d7482ac4
Paul M Shumate <+>
2024-01-31 11:49:40 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+
+  }
+  
+  const gifts = ["teddy bear", "drone", "doll"];
+  
+  function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+    }
+  }
+  
+  wrapGifts(gifts);
+  
+  function writeCards(names, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  countDown(10);
+  
+  module.exports = {
+    countDown,
+  };
